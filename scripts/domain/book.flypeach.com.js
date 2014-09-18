892f003cefb6656d73c93c1af31a1098429f490c
vaward <vaward.xie@gmail.com>
2014-09-18 10:50:32 +0800

    删除document.write

--- scripts/domain/book.flypeach.com.js
+++ scripts/domain/book.flypeach.com.js
@@ -5,7 +5,6 @@ var CHECKData = function () {
       for (e = 0; e < arrDestination.length; e++)
       {
         var a = arrDestination[e].split('|');
-        document.write(a[0] + '-' + a[2] + '<br/>');
         result += a[0] + '-' + a[2] + ',';
       }
     return result;
