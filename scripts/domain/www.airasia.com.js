857f1e9a3312a70d2352167fb23054439295c477
vaward <vaward.xie@gmail.com>
2014-09-18 10:10:10 +0800

    添加亚洲航空域名判断

--- scripts/domain/www.airasia.com.js
+++ scripts/domain/www.airasia.com.js
@@ -1,11 +1,13 @@
 var CHECKData = function() {	
 	this.getData = function(domain) {
-		var result;		
-		var routes_table = SKYSALES.Resource.marketInfo.MarketList;
-		for (ap in routes_table) {
-			dep = ap;			
-			for (var i = 0; i < routes_table[dep].length; i++) {				
-				result += dep + '-' + routes_table[dep][i].code + ',';
+		var result;
+		if (domain == 'www.airasia.com'){
+			var routes_table = SKYSALES.Resource.marketInfo.MarketList;
+			for (ap in routes_table) {
+				dep = ap;			
+				for (var i = 0; i < routes_table[dep].length; i++) {				
+					result += dep + '-' + routes_table[dep][i].code + ',';
+				}
 			}
 		}
 		return result;
